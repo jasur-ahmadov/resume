447d1ddf65ae2a60785face3fd9fa95e7407356a
Jasur <casur.ahmadov@khazar.org>
2023-12-31 00:04:59 +0400

    next commit

---
+++ ResumeWebApp/src/main/java/com/company/resume/main/JakartaRestConfiguration.java
@@ -0,0 +1,9 @@
+package com.company.resume.main;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+
+}
